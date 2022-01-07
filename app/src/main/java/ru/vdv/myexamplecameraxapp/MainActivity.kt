fe1853e6d06ee4e780f3e33c17a63efe2944992d
Dmitriy <stdimensiy@yandex.ru>
2022-01-08 01:22:46 +0300

    Изм 0.2 Create the viewfinder layout and MainActivity code - Объявлены и инициализированы переменные рабочей директориии, экзекьютора и файла самой картинки (класс ImageCapture) - функции takePhoto() и startCamera() объявлены но не реализованы пока - создана функция получения рабочей директории - создана функция определения наличия разрешений для использования камеры. - переопределен метод onDestroy() в котором командуем экзекьютору завершить выполнение текущей задачи и новые не принимать. - создан компаньон объект с константами

--- app/src/main/java/ru/vdv/myexamplecameraxapp/MainActivity.kt
+++ app/src/main/java/ru/vdv/myexamplecameraxapp/MainActivity.kt
@@ -1,11 +1,78 @@
 package ru.vdv.myexamplecameraxapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Запрос разрешения камеры
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Навешивание слушателя на кнопку
+        camera_capture_button.setOnClickListener { takePhoto() }
+
+        // инициализация переменных
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+    }
+
+    private fun takePhoto() {
+        TODO("Not yet implemented")
+    }
+
+    private fun startCamera() {
+        TODO("Not yet implemented")
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        return REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                baseContext, it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        cameraExecutor.shutdown()
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
