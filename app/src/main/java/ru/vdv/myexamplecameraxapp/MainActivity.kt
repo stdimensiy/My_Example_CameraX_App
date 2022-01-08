6749291b35d2f5378edcd47fdd5eed421a7de01c
Dmitriy <stdimensiy@yandex.ru>
2022-01-08 13:47:25 +0300

    Изм 0.5 Implement ImageCapture use case - в рамках реализации метода takePhoto выполнена проверка на наличие стабильной ссылки на объект захвата изображения, создается и уникально именуется сам файл сохранения изображения назначается слушатель события захвата и сохранения изображения в колбэк -  модифицирована функция startCamera() в части инициализации объекта захвата изображения и передачи его провайдеру камеры в качетве параметра метода bindToLifecycle

--- app/src/main/java/ru/vdv/myexamplecameraxapp/MainActivity.kt
+++ app/src/main/java/ru/vdv/myexamplecameraxapp/MainActivity.kt
@@ -3,18 +3,22 @@ package ru.vdv.myexamplecameraxapp
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -46,7 +50,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
-        TODO("Not yet implemented")
+        // получаем стабильную ссылку на
+        val imageCapture: ImageCapture = imageCapture ?: return
+        // создаем файл с отметкой времени для зранения изображения
+        val photoFile: File = File(
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT,
+                Locale.ROOT
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        // настройка слушателя события захвата изображения
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Фотография успешно сохранена: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+            }
+        )
     }
 
     private fun startCamera() {
@@ -58,13 +89,14 @@ class MainActivity : AppCompatActivity() {
                 val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
                 val preview = Preview.Builder().build()
                     .also { it.setSurfaceProvider(viewFinder.surfaceProvider) }
+                imageCapture = ImageCapture.Builder().build()
                 //выбираем заднюю камеру по умолчанию
                 val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
                 try {
                     //отвязка юзкейсов камеры перд привязкой
                     cameraProvider.unbindAll()
                     //привязка юзкейса к камере
-                    cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
                 } catch (exc: Exception) {
                     Log.e(TAG, "Привязка использования камеры не удалась!")
                 }
